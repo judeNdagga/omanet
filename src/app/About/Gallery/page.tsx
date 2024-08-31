5e9bd403ad65b6d1b5e451e2e138fb1fb050f0fc
judeNdagga <51657076+judeNdagga@users.noreply.github.com>
2024-08-31 17:25:31 +0300

    about fix trial

--- src/app/About/Gallery/page.tsx
+++ src/app/About/Gallery/page.tsx
@@ -3,12 +3,12 @@ import image1 from "/public/images/Gallery/20201030_121306.jpg";
 import image2 from "/public/images/Gallery/20201030_121326.jpg";
 import image3 from "/public/images/Gallery/20230408_104111.jpg";
 import image4 from "/public/images/Gallery/20230408_104118.jpg";
-import image5 from "/public/images/Gallery/DSC03099.jpg";
-import image6 from "/public/images/Gallery/DSC03103.jpg";
-import image7 from "/public/images/Gallery/DSC03105.jpg";
-import image8 from "/public/images/Gallery/DSC03108.jpg";
-import image9 from "/public/images/Gallery/DSC03114.jpg";
-import image10 from "/public/images/Gallery/DSC03131.jpg";
+// import image5 from "/public/images/Gallery/DSC03099.jpg";
+// import image6 from "/public/images/Gallery/DSC03103.jpg";
+// import image7 from "/public/images/Gallery/DSC03105.jpg";
+// import image8 from "/public/images/Gallery/DSC03108.jpg";
+// import image9 from "/public/images/Gallery/DSC03114.jpg";
+// import image10 from "/public/images/Gallery/DSC03131.jpg";
 
 import { CardBody, CardContainer, CardItem } from "@/app/components/ui/3d-card";
 export default function Gallery() {
@@ -20,14 +20,14 @@ export default function Gallery() {
       {image : image2},
       {image : image3},
       {image : image4},
-      {image : image5},
-      {image : image6},
-      {image : image7},
-      {image : image8},
-      {image : image9},
-      {image : image10},
-      {image : image10},
-      {image : image10},
+      // {image : image5},
+      // {image : image6},
+      // {image : image7},
+      // {image : image8},
+      // {image : image9},
+      // {image : image10},
+      // {image : image10},
+      // {image : image10},
       
       
       
