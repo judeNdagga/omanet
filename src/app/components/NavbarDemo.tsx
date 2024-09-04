737ab4faa4b7e07d0fe47ba496a1d7479c2d8519
judeNdagga <51657076+judeNdagga@users.noreply.github.com>
2024-09-04 12:37:29 +0300

    fix navbar links

--- src/app/components/NavbarDemo.tsx
+++ src/app/components/NavbarDemo.tsx
@@ -60,13 +60,13 @@ function Navbar({ className }: { className?: string }) {
           <div className="text-sm grid sm:grid-cols-2 gap-10 p-4">
             <ProductItem
               title="Organic Wine"
-              href="/Products/OrganicWine/BuyWine"
+              href="/Products"
               src="https://images.unsplash.com/photo-1610371800811-a7d2a7b6b3e8?q=80&w=1963&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
               description="Organic wine with a rich taste"
             />
             <ProductItem
               title="Fresh Cherry Tomatoes"
-              href="/Products/Tomatoes/BuyTomatoes"
+              href="/Products"
               src="https://images.unsplash.com/photo-1589190051962-0b138f75d3c9?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
               description="Fresh cherry tomatoes from organic farms"
             />
@@ -78,7 +78,7 @@ function Navbar({ className }: { className?: string }) {
             />
             <ProductItem
               title="Local Eggs"
-              href="/Products/Eggs"
+              href="/Products"
               src="https://images.unsplash.com/photo-1586802990181-a5771596eaea?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
               description="Delicious organic eggs"
             />
